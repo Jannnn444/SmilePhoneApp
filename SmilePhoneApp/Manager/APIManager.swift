22a2858ddbdafee2e898822068467dc4fd804f22
Janus <Janus.Haung@hualiteq.com>
2025-08-12 16:30:33 +0800

    Add the file folders

---
+++ SmilePhoneApp/Manager/APIManager.swift
@@ -0,0 +1,11 @@
+//
+//  APIManager.swift
+//  SmilePhoneApp
+//
+//  Created by Hualiteq International on 2025/8/12.
+//
+
+import Foundation
+class APIManager {
+    init() {}
+}
---
+++ SmilePhoneApp/Manager/WebsocketManager.swift
@@ -0,0 +1,12 @@
+//
+//  WebsocketManager.swift
+//  SmilePhoneApp
+//
+//  Created by Hualiteq International on 2025/8/12.
+//
+
+import Foundation
+
+class WebsocketManager {
+    init() {}
+}
